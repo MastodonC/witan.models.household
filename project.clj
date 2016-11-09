3b5f5b0a36fe56174ba8e95920d0b73b1ad6b5b1
Eleonore9 <eleonore.mayola@ymail.com>
2016-11-09 10:26:56 +0000

    Move `environ` to dev deps

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
                  [net.mikera/core.matrix "0.55.0"]
                  [witan.workspace-api "0.1.20"]
                  [prismatic/schema "1.1.3"]
-                 [org.clojure/data.csv "0.1.3"]
-                 [environ "1.0.1"]]
+                 [org.clojure/data.csv "0.1.3"]]
   :profiles {:dev {:dependencies [[witan.workspace-executor "0.2.6"
-                                    :exclusions [witan.workspace-api]]]}
+                                   :exclusions [witan.workspace-api]]
+                                  [environ "1.0.1"]]}
              :data {:source-paths ["src-data"]
                     :dependencies [[amazonica "0.3.73"]
                                    [me.raynes/fs "1.4.6"]]}}
